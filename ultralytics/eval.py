658807f6874ecbaeafedb139c598387f200b8a80
William Guimont-Martin <william.guimont-martin@norlab.ulaval.ca>
2025-08-29 11:47:55 -0400

    wip

--- ultralytics/eval.py
+++ ultralytics/eval.py
@@ -1,45 +1,66 @@
 import argparse
-import numpy as np
-import json
+import logging
+import re
 from collections import defaultdict
+from pathlib import Path
 
+import numpy as np
+import pycocotools.mask as mask_utils
 import torch
-from pycocotools import mask as mask_utils
+import yaml
 from torchmetrics.detection import MeanAveragePrecision
 
 from ultralytics import YOLO
 
+STREAM = False
+RETINA_MASK = False
+CONF = 0.25
+IOU = 0.7
+TTAUGMENT = False
+# Enable class-agnostic NMS (useful if class overlap is common)
+AGNOSTIC_NMS = False
+SMALL_SIZE = False
+
+RUN_NAME_REGEX = re.compile(r'[a-z0-9.-]*\.json_([a-z-0-9]*)_kfold_([0-9])\.yaml')
+
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Evaluate segmentation mAP (segm) using torchmetrics MeanAveragePrecision")
-    parser.add_argument('--model', type=str, default='yolo11s-seg.pt', help='Path to YOLO segmentation model weights')
-    parser.add_argument('--data', type=str, required=True, help='Path to dataset YAML or directory/txt of images')
-    parser.add_argument('--json', type=str, required=True, help='Path to dataset JSON (from the split to evaluate on)')
-    parser.add_argument('--split', type=int, required=True, help='Image split identifier')
-    parser.add_argument('--conf', type=float, default=0.25, help='Confidence threshold for predictions')
+    parser = argparse.ArgumentParser(description="Generate evaluation information")
+    parser.add_argument('--run-path', required=True, help='Path to result folder')
     args = parser.parse_args()
 
-    model_pt = args.model
-    data_yaml_path = args.data
-    data_json_path = args.json
-    split = args.split
-    conf = args.conf
+    if SMALL_SIZE:
+        logging.warning('PUT SMALL_SIZE TO False FOR REAL EVAL')
 
-    with open(data_json_path, 'r') as f:
-        data = json.load(f)
+    run_path = Path(args.run_path)
+    args_path = run_path / 'args.yaml'
+    weights_path = run_path / 'weights'
+    run_name = run_path.name
+    res = re.search(RUN_NAME_REGEX, run_name)
+    split_num, kfold_index = res[1], res[2]
 
-    model = YOLO(args.model)
+    with open(args_path, 'r') as f:
+        args = yaml.safe_load(f)
 
-    imgsz = [1456, 1092]
-    if split == 16:
-        imgsz = [728, 546]
+    model = weights_path / 'best.pt'
+    imgsz = 640 if SMALL_SIZE else args['imgsz']
+    split_path = Path(f'/datasets/vhr-silva/subsets/{split_num}/split_{kfold_index}.json')
 
-    all_images = [img['file_name'].replace('tif', f'tif-{split}', 1) for img in data['images']]
-    results = model(all_images, imgsz=imgsz, device='cuda', retina_masks=True)
+    with open(split_path, 'r') as f:
+        data = yaml.safe_load(f)
+
+    model = YOLO(model)
+    model.eval()
+
+    anns_per_img = defaultdict(list)
+    for ann in data['annotations']:
+        anns_per_img[ann['image_id']].append(ann)
 
     preds = []
-    for img, res in zip(data['images'], results):
+    targets = []
+    for img in sorted(data['images'], key=lambda d: d['id']):
+        res = model(img['file_name'], imgsz=imgsz, device='cuda', stream=STREAM, retina_masks=RETINA_MASK, conf=CONF,
+                    iou=IOU, augment=TTAUGMENT, agnostic_nms=AGNOSTIC_NMS)[0]
         masks = [] if res.masks is None else res.masks
         masks = [m.data for m in masks]
         masks = torch.vstack(masks) if len(masks) > 0 else torch.tensor([])
@@ -47,14 +68,9 @@ def main():
             masks=masks.to(torch.uint8).cpu(),
             scores=res.boxes.conf.cpu(),
             labels=res.boxes.cls.detach().clone().cpu().to(torch.uint8),
-            image_id=img['id']
+            speed=res.speed
         ))
 
-    anns_per_img = defaultdict(list)
-    for ann in data['annotations']:
-        anns_per_img[ann['image_id']].append(ann)
-    targets = []
-    for img in data['images']:
         anns = anns_per_img[img['id']]
         masks = [mask_utils.decode(ann['segmentation']) for ann in anns]
         labels = [ann['category_id'] for ann in anns]
@@ -63,7 +79,6 @@ def main():
             labels=torch.tensor(labels, dtype=torch.long)
         ))
 
-    # data['preds'] = preds
     metric = MeanAveragePrecision(iou_type='segm')
     metric.update(preds, targets)
     metrics = metric.compute()
