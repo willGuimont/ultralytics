52cd676dbdb4d449f71f26019b2c97d9b39cf79f
William Guimont-Martin <william.guimont-martin@norlab.ulaval.ca>
2025-09-01 11:56:33 -0400

    new eval

--- ultralytics/eval.py
+++ ultralytics/eval.py
@@ -18,13 +18,14 @@ from ultralytics import YOLO
 STREAM = False
 RETINA_MASK = True
 CONF = 0.25
-IOU = 0.7
+IOU = 0.5
 TTAUGMENT = False
 # Enable class-agnostic NMS (useful if class overlap is common)
-AGNOSTIC_NMS = True
+AGNOSTIC_NMS = False
 
 SMALL_SIZE = False
 REVERSE_IMGSZ = True
+HARDCODED_IMGSZ = (1092, 1456)
 
 RUN_NAME_REGEX = re.compile(r'[a-z0-9.-]*\.json_([a-z-0-9]*)_kfold_([0-9])\.yaml')
 
@@ -44,7 +45,6 @@ def overlay_masks(base_img, masks_tensor, alpha=0.4, seed=0):
         if mask.shape != (h, w):  # skip size mismatch
             logging.warning(f'Could not overlay mask, size miss match (mask={mask.shape} != {(h, w)}=img')
             exit(1)
-            continue
         color = rng.integers(0, 255, size=3, dtype=np.uint8)
         color_mask = np.zeros_like(out)
         color_mask[mask.astype(bool)] = color
@@ -80,14 +80,25 @@ def compute_map(run_path):
         args = yaml.safe_load(f)
 
     model = weights_path / 'best.pt'
+
     imgsz = 640 if SMALL_SIZE else args['imgsz'][::-1]
+    if HARDCODED_IMGSZ is not None:
+        logging.warning(f'Using hardcoded value for imgsz: {HARDCODED_IMGSZ}')
+        imgsz = HARDCODED_IMGSZ
+
     split_path = Path(f'/datasets/vhr-silva/subsets/{split_num}/split_{kfold_index}.json')
 
     with open(split_path, 'r') as f:
         data = yaml.safe_load(f)
 
-    model = YOLO(model)
+    try:
+        model = YOLO(model)
+    except FileNotFoundError:
+        print(f'Error loading {run_path}')
+        return None
     model.eval()
+
+    model.val(data=args['data'], split='test')
 
     anns_per_img = defaultdict(list)
     for ann in data['annotations']:
--- ultralytics/eval_all.py
+++ ultralytics/eval_all.py
@@ -26,7 +26,7 @@ def average_dict(dicts: List[Dict]):
 if __name__ == '__main__':
     logging.disable()
 
-    root = Path('/datasets/yolo_runs')
+    root = Path('/datasets/yolo_runs2')
     all_folds = sorted(f for f in root.iterdir() if f.is_dir())[::-1]
 
     grouped_by_model_folds = defaultdict(list)
@@ -35,9 +35,15 @@ if __name__ == '__main__':
         model, split, kfold = res[1], res[2], res[3]
         grouped_by_model_folds[(model, split)].append(fold)
 
+    for k, v in grouped_by_model_folds.items():
+        assert len(v) == 5, f'Error for {k}, has {len(v)} != 5'
+
     for (model, split), paths in grouped_by_model_folds.items():
-        folds = [compute_map(path)['map'] for path in paths]
-        avg_metric = average_dict(folds)
+        folds = [compute_map(path) for path in paths]
+        if any(f is None for f in folds):
+            print(f'Could not compute for ({model, split})')
+            continue
+        avg_metric = average_dict(list(f['map'] for f in folds))
         print('-' * 20)
         print(model, split)
         print(avg_metric)
